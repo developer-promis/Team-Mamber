e99640dd38eeb16ce3b156188eaef0b367ac608a
PROMIS BASUNIA <126019991+developer-promis@users.noreply.github.com>
2023-07-08 18:39:25 +0600

    Add files via upload

---
+++ app/data.js
@@ -0,0 +1,49 @@
+const team_members_data = [
+  {
+    id: 1,
+    name: "Mon Mohon",
+    image: `./assets/images/tem_images/1.png`,
+    about: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, quas delectus laboriosam laudantium reiciendis rem`,
+    // social links
+    facebook: `https://facebook.com/zahir`,
+    instagram: `https://instagram.com/zahir`,
+    linkeding: `https://linkeding.com/zahir`,
+    twitter: `https://twitter.com/zahir`,
+  },
+  {
+    id: 2,
+    name: "JOy",
+    image: `./assets/images/tem_images/2.png`,
+    about: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, quas delectus laboriosam laudantium reiciendis rem`,
+    // social links
+    facebook: `https://facebook.com/ebrahim`,
+    instagram: `https://instagram.com/ebrahim`,
+    linkeding: `https://linkeding.com/ebrahim`,
+    twitter: `https://twitter.com/ebrahim`,
+  },
+  {
+    id: 3,
+    name: "Promis kumar",
+    image: `./assets/images/tem_images/3.png`,
+
+    about: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, quas delectus laboriosam laudantium reiciendis rem`,
+    // social links
+    facebook: `https://facebook.com/imran`,
+    instagram: `https://instagram.com/imran`,
+    linkeding: `https://linkeding.com/imran`,
+    twitter: `https://twitter.com/imran`,
+  },
+  {
+    id: 4,
+    name: "Islam Rakib",
+    image: `./assets/images/tem_images/4.png`,
+
+    about: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, quas delectus laboriosam laudantium reiciendis rem`,
+    // social links
+    facebook: `https://facebook.com/asadur`,
+    instagram: `https://instagram.com/asadur`,
+    linkeding: `https://linkeding.com/asadur`,
+    twitter: `https://twitter.com/asadur`,
+  },
+  
+];
